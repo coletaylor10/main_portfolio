332cd90187befc5d385e6716a35112b761c20ea2
coletaylor10 <cole10.taylor@gmail.com>
2023-01-19 23:40:59 +0000

    cosmetic changes

--- pages/random.jsx
+++ pages/random.jsx
@@ -38,17 +38,17 @@ const random = () => {
             <Link href="https://github.com/coletaylor10/RandomPass">Code</Link>
           </button>
         </div>
-        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+        <div className="col-span-4 md:col-span-1 shadow-xl border border-[#ffef0f] rounded-xl py-4">
           <div className="p-2">
             <p className="text-center font-bold pb-2">Technologies</p>
             <div className="grid grid-cols-3 md:grid-cols-1">
-              <p className="text-gray-600 py-2 flex items-center">
+              <p className="text-white py-2 flex items-center">
                 <RiRadioButtonFill className="pr-1" /> Html
               </p>
-              <p className="text-gray-600 py-2 flex items-center">
+              <p className="text-white py-2 flex items-center">
                 <RiRadioButtonFill className="pr-1" /> CSS
               </p>
-              <p className="text-gray-600 py-2 flex items-center">
+              <p className="text-white py-2 flex items-center">
                 <RiRadioButtonFill className="pr-1" /> Javascript
               </p>
             </div>
