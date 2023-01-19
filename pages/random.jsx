cf4d77985976560059de6e1af7f7e33e5ec90b9e
coletaylor10 <cole10.taylor@gmail.com>
2023-01-19 23:45:51 +0000

    More cosmetic changes

--- pages/random.jsx
+++ pages/random.jsx
@@ -24,7 +24,7 @@ const random = () => {
       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
         <div className="col-span-4">
           <p>Project</p>
-          <h2>Overview</h2>
+          <h2 className="py-4">Overview</h2>
           <p>
             I have created a random password generator app that generates unique
             and secure passwords for users. The app offers options for password
