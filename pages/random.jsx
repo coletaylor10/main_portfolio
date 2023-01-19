6dd5a58c5ff72f1a56f767b2391fa4b9095840ce
coletaylor10 <cole10.taylor@gmail.com>
2023-01-19 23:32:30 +0000

    Random Pass Update

--- pages/random.jsx
+++ pages/random.jsx
@@ -17,8 +17,8 @@ const random = () => {
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-4">
-          <h2 className="py-2">Property Finder</h2>
-          <h3>React JS / Tailwind / Firebase</h3>
+          <h2 className="py-2">Random Password Generator</h2>
+          <h3>Html, CSS, JS, AWS</h3>
         </div>
       </div>
       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
@@ -26,43 +26,36 @@ const random = () => {
           <p>Project</p>
           <h2>Overview</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum
-            suscipit non quam, maxime officia nihil nobis eveniet aspernatur
-            eius vel, distinctio optio labore explicabo cupiditate, ut minima
-            aliquam! Dicta incidunt consectetur ea ad possimus dolorum inventore
-            esse accusantium eveniet! Aliquam ex officiis architecto suscipit
-            laudantium perspiciatis impedit placeat laborum.
+            I have created a random password generator app that generates unique
+            and secure passwords for users. The app offers options for password
+            length and symbols.You also have the ability to export the generated
+            password to the clipboard. I made this using the most basic web development technologies.
           </p>
-          <button className=" px-8 py-2 mt-4 mr-8">Demo</button>
-          <button className=" px-8 py-2 mt-4 ">Code</button>
+          <button className=" px-8 py-2 mt-4 mr-8">
+            <Link href="https://main.d3m0v5h3rr3fi6.amplifyapp.com/">Demo</Link>
+          </button>
+          <button className=" px-8 py-2 mt-4 ">
+            <Link href="https://github.com/coletaylor10/RandomPass">Code</Link>
+          </button>
         </div>
         <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
           <div className="p-2">
             <p className="text-center font-bold pb-2">Technologies</p>
             <div className="grid grid-cols-3 md:grid-cols-1">
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> React
+                <RiRadioButtonFill className="pr-1" /> Html
               </p>
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Tailwind
+                <RiRadioButtonFill className="pr-1" /> CSS
               </p>
               <p className="text-gray-600 py-2 flex items-center">
                 <RiRadioButtonFill className="pr-1" /> Javascript
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Firebase
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Google API
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Zillow API
               </p>
             </div>
           </div>
         </div>
         <Link href="/#projects">
-            <p className="underline cursor-pointer text-[#ffef0f]">Back</p>
+          <p className="underline cursor-pointer text-[#ffef0f]">Back</p>
         </Link>
       </div>
     </div>
